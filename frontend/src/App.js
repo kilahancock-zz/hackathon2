8a336372d97ecc9c6017d49e33a0c017493e562d
Kila Hancock <kilahancock@gmail.com>
2020-07-27 14:05:40 -0400

    adds file structure and sets up bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,55 @@
-import React, { useEffect } from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Home from './containers/Home.js';
+import Community from './containers/Community.js';
+import Organizations from './containers/Organizations.js';
+import NavBar from './components/NavBar.js'
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-  useEffect(() => {
-    fetch('http://localhost:3000/meta')
-      .then(response => response.json())
-      .then(data => console.log(data[0]));
-  }, [])
-
-  return (
-    <div className="App">
-      <h1>home</h1>
-    </div>
-  );
+    this.state = {
+      current_page: 'home'
+    };
+  }
+//maybe we should consider react router?
+  render() {
+    switch(this.state.current_page) {
+      case 'home': {
+        return (
+          <div className="App">
+            <NavBar />
+            <Home />
+          </div>
+        )
+      }
+      case 'community': {
+        return (
+          <div className="App">
+            <NavBar />
+            <Community />
+          </div>
+        )
+      }
+      case 'organizations': {
+        return (
+          <div className="App">
+            <NavBar />
+            <Organizations />
+          </div>
+        )
+      }
+      default: {
+        return (
+          <div className="App">
+            <NavBar />
+            <Home />
+          </div>
+        )
+      } 
+    }
+  }
 }
 
 export default App;
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() {
+      return (
+        <p>NavBar</p>
+      )
+    }
+}
+
+export default NavBar;
