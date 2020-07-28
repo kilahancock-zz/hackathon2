1457611a2729185536d5fc280e2b2ac07e4ec4f6
kev-begic <kbegic@c02r70zhg8wm.localdomain>
2020-07-28 11:45:18 -0400

    saving for pull.

--- sql/config.sql
+++ sql/config.sql
@@ -1,4 +1,21 @@
 create table Test_Table (
     id INT AUTO_INCREMENT PRIMARY KEY
 )
-    
+-- create table Person (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     username VARCHAR(15),
+--     password CHAR(64),
+--     email VARCHAR(50),
+--     zipcode char(5)
+-- )
+-- CREATE TABLE Resource (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     pid INT,
+--     rname VARCHAR(15),
+--     rtype VARCHAR(15),
+--     request BOOLEAN,
+--     desc VARCHAR(100),
+--     zipcode char(5)
+-- )
+-- INSERT INTO Resource (id,pid,rname,rtype,request,dsc,zipcode)
+-- VALUES (1,1,"Dinner","Veggie", false,"Food for those who don't eat meat.", "00727")
