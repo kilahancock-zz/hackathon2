aeec0ddd8c099cd02031a25091aea341e7c87580
Javier Hernández Novoa <javier.hernandez7@upr.edu>
2020-07-29 11:36:49 -0400

    Commit for merging with kevinB

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	//"encoding/json"
 	"net/http"
 	"os"
@@ -9,9 +8,8 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/kelseyhightower/envconfig"
-	"github.com/rs/zerolog"
 	"github.com/kilahancock/hackathon2/backend/routes"
-
+	"github.com/rs/zerolog"
 )
 
 var (
@@ -35,24 +33,14 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/health", nutrishare.Health)
-
 	router.HandleFunc("/login", nutrishare.Login)
+	router.HandleFunc("/resource/", nutrishare.Resource)
 
 	l.Info().Msg("server running on port 3000")
 	err := http.ListenAndServe(":3000", router)
 	if err != nil {
 		l.Fatal().Err(err).Msg("unable to start server")
 	}
-}
-
-func Login(w http.ResponseWriter, r *http.Request) {
-	var p []byte
-
-	r.Body.Read(p)
-
-	_ = json.NewEncoder(w).Encode(map[string]interface{}{
-		"got": string(p),
-	})
 }
 
 func initDB(){
--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,16 +2,9 @@ package nutrishare
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
-
-func main() {
-	
-}
-
-func Stuff(){
-
-}
 
 func Health(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewEncoder(w).Encode(map[string]interface{}{
@@ -20,11 +13,35 @@ func Health(w http.ResponseWriter, r *http.Request) {
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
-	var p []byte
+	type person struct{
+		id int
+		pid int
+		rname string
+		rtype string
+		request bool
+		dsc string
+		zipcode string
+	}
+	p := person{id: 1, pid: 1, rname: "Tacos", rtype: "dinner",request: false,dsc: "Good food.", zipcode: "00727"}
 
-	r.Body.Read(p)
+	err := json.NewEncoder(w).Encode(p)
+	if err != nil{
+		fmt.Sprintf("We blew up...")
+	}
 
-	_ = json.NewEncoder(w).Encode(map[string]interface{}{
-		"got": string(p),
-	})
+	w.Header().Set("content-type", "application/json")
+
+	w.WriteHeader(http.StatusOK)
+
+}
+
+func Resource(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case http.MethodPost:
+		// TODO
+	case http.MethodGet:
+		// TODO
+	}
+
 }
