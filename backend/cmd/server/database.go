a524bd00709d7fcd6c5535495a9d0b6730387d37
Javier Hernández Novoa <javier.hernandez7@upr.edu>
2020-07-29 17:22:33 -0400

    Added attempt to save person to the db. Getting error in database.go

--- backend/cmd/server/database.go
+++ backend/cmd/server/database.go
@@ -3,10 +3,11 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/rs/zerolog/log"
 )
 
 func newDB(user, pass string) (*sql.DB, error) {
-	dsn := fmt.Sprintf("%s:%s@tcp(mysql:3306)/?parseTime=true", user, pass)
+	dsn := fmt.Sprintf("%s:%s@tcp(mysql:3306)/testDB?parseTime=true", user, pass)
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return nil, err
@@ -14,4 +15,25 @@ func newDB(user, pass string) (*sql.DB, error) {
 
 	return db, nil
 }
+
+func SavePerson(p Person) (int64, error) {
+
+	log.Info().Msg(fmt.Sprintf("Person: %+v", p))
+	res, err := DB.Exec(`INSERT INTO Person(username,password,email,zipcode)
+		VALUES ($1,$2,$3,$4)`, p.Username, p.Password, p.Email, p.Zipcode)
+
+	// This error is triggering with 'No database selected'
+	if err != nil{
+		return 0, err
+	}
+	pid, err := res.LastInsertId()
+
+	return pid, err
+}
+
+//func SaveCustomer(username string) error {
+//	db := GetDB()
+//	_, err := db.Exec("INSERT INTO customer (username) VALUES($1)", username)
+//	return err
+//}
 
--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"os"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/kelseyhightower/envconfig"
-	"github.com/kilahancock/hackathon2/backend/routes"
 	"github.com/rs/zerolog"
 )
 
@@ -22,6 +23,10 @@ var cfg struct {
 		Pass string `required:"true" split_words:"true"`
 	}
 }
+
+// To refactor
+var DB *sql.DB
+
 type Summary struct {
 	Username string `json:"username"`
 	Email string `json:"email"`
@@ -33,15 +38,28 @@ func main() {
 		l.Fatal().Err(err).Msg("error parsing config")
 	}
 
+	// Sleep to give enough time for SQL to start running
+	time.Sleep(2*time.Second)
+	// initialize database
+	var err error
+	DB, err = newDB(cfg.DB.User, cfg.DB.Pass)
+	if err != nil {
+		l.Fatal().Err(err).Msg("could not create db connection")
+	}
+	err = DB.Ping()
+	if err != nil {
+		l.Fatal().Err(err).Msg("unable to ping")
+	}
+
 	router := mux.NewRouter()
 
-	router.HandleFunc("/health", nutrishare.Health)
-	router.HandleFunc("/login", nutrishare.Login)
-	router.HandleFunc("/signup", nutrishare.PersonCreate)
-	router.HandleFunc("/resource", nutrishare.Resource)
+	router.HandleFunc("/health", Health)
+	router.HandleFunc("/login", Login)
+	router.HandleFunc("/signup",PersonCreate)
+	router.HandleFunc("/resource", Resource)
 
 	l.Info().Msg("server running on port 3000")
-	err := http.ListenAndServe(":3000", router)
+	err = http.ListenAndServe(":3000", router)
 	if err != nil {
 		l.Fatal().Err(err).Msg("unable to start server")
 	}
@@ -57,10 +75,4 @@ func initDB(){
 	if err != nil {
 		l.Fatal().Err(err).Msg("unable to ping")
 	}
-}
-
-func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	(*w).Header().Set("Content-Type", "application/json")
 }
