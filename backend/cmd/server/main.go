5222b3acbf493ac1f1ffbb629564889fc20adc37
kev-begic <kbegic@c02r70zhg8wm.localdomain>
2020-07-29 13:21:14 -0400

    Cleaned up main.

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 	"os"
-	"io/ioutil"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -37,46 +35,6 @@ func main() {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/healthk", func(w http.ResponseWriter, rr *http.Request) {
-		enableCors(&w);
-		_ = json.NewEncoder(w).Encode(map[string]interface{}{
-			"healthy": "ok",
-		})
-	})
-
-	router.HandleFunc("/logink", func(w http.ResponseWriter, rr *http.Request) {
-		enableCors(&w);
-		// var test_summary Summary;
-		// err := json.NewDecoder( rr.Body ).Decode( &test_summary );
-		// if err != nil {
-		// 	l.Info().Msg( "failed decoder" );
-		// 	panic(err);
-		// }
-		// l.Info().Msg( test_summary.Username );
-		// l.Info().Msg( test_summary.Email );
-
-		// ! This here worked, but couldn't unmarshall correctly
-		b, err := ioutil.ReadAll(rr.Body)
-		if err != nil {
-			panic(err)
-		}
-		// "message":"{\"username\":\"b\",\"email\":\"a@gmail.com\"}"}
-		l.Info().Msg(string(b));
-
-		// TODO: Figure out how to write body into a struct
-		var data map[string]interface{}
-		err3 := json.Unmarshal(b, &data)
-		if err3 != nil {
-			l.Info().Msg("Error #3");
-			panic(err3)
-		}
-
-		_ = json.NewEncoder(w).Encode(map[string]interface{}{
-			"login": "ok",
-			"username": data,
-		})
-	})
-
 	router.HandleFunc("/health", nutrishare.Health)
 	router.HandleFunc("/login", nutrishare.Login)
 	router.HandleFunc("/signup", nutrishare.PersonCreate)
@@ -106,18 +64,3 @@ func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	(*w).Header().Set("Content-Type", "application/json")
 }
-
-// ! New Implementation
-/*
-decoder := json.NewDecoder( rr.Body );
-decoder.DisallowUnknownFields();
-
-var test_summary Summary;
-err2 := decoder.Decode( &test_summary );
-if err2 != nil {
-	l.Info().Msg( "failed decoder" );
-	panic(err2);
-}
-l.Info().Msg( test_summary.Username );
-l.Info().Msg( test_summary.Email );
-*/
